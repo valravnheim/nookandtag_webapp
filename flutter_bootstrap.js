7e7ca216e1a516e4cb8b5bf04370b0323418bd6b
val <valravnheim@gmail.com>
2025-10-18 21:54:20 +1300

    added correct map

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2305645306"
+    serviceWorkerVersion: "424524617"
   }
 });
