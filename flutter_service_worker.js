aa4b08260c135568cdceb146a65c4c1dfc202a03
val <valravnheim@gmail.com>
2025-10-18 20:48:45 +1300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -124,7 +124,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6f9e831b013342a03581212d24506738
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "eaf77d33d20b4f352c93d0fae20e4b2c",
+"flutter_bootstrap.js": "6c383fdf53e0ff9179284fa93fb0bb5d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
