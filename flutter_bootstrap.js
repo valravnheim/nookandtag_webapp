32e8a70feda5ad349976146abc19104eda8670d7
val <valravnheim@gmail.com>
2025-10-19 23:12:57 +1300

    trying to get instagram logo to show on mobile browser

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1454531686"
+    serviceWorkerVersion: "2719785086"
   }
 });
