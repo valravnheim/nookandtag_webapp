a88da474eda573dda7248729463b03779ee4098a
val <valravnheim@gmail.com>
2025-10-20 14:51:12 +1300

    v1.0.5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4023542100"
+    serviceWorkerVersion: "3054720348"
   }
 });
