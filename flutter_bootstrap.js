19fdacf4474e52cdfa8dfb48c0e5ef5491a4294b
val <valravnheim@gmail.com>
2026-08-29 16:14:46 +1200

    ...

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1721813543" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "666468577" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
